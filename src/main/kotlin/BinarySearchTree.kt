cd898dfb1651bd2feafa98de0ec07174880dc45a
daiki <gigabowser@hotmail.co.jp>
2026-01-06 23:22:04 +0900

    二分探索木のサンプルの削除を実装

--- src/main/kotlin/BinarySearchTree.kt
+++ src/main/kotlin/BinarySearchTree.kt
@@ -12,6 +12,10 @@ fun main() {
     println(tree.search(3))
     println(tree.search(0))
     println(tree.search(9))
+
+    tree.remove(1)
+    println(tree.search(1))
+    println(tree)
 }
 
 data class BinarySearchTreeNode<K : Comparable<K>, V> (
@@ -36,6 +40,73 @@ class BinarySearchTree<K : Comparable<K>, V> {
 
     fun search(key: K): V? {
         return search(root, key)
+    }
+
+    fun remove(key: K): Boolean {
+        if (root == null) {
+            return false
+        }
+
+        return remove(null, root, key)
+    }
+
+    private fun remove(parent: BinarySearchTreeNode<K, V>?, node: BinarySearchTreeNode<K, V>?, key: K): Boolean {
+        if(node == null) {
+            return false
+        }
+
+        return if(node.key == key) {
+            if(node.left == null) {
+                if(parent == null) {
+                    root = node.right
+                } else if(parent.key > key) {
+                    parent.left = node.right
+                } else {
+                    parent.right = node.right
+                }
+            } else if(node.right == null) {
+                if(parent == null) {
+                    root = node.left
+                } else if(parent.key > key) {
+                    parent.left = node.left
+                } else {
+                    parent.right = node.left
+                }
+            } else {
+                val (parentOfLeftLargest, leftLargest) = findLargest(node, node.left!!)
+
+                if (leftLargest.key == node.left!!.key) {
+                    leftLargest.right = node.right
+                } else {
+                    parentOfLeftLargest.right = leftLargest.left
+
+                    leftLargest.left = node.left
+                    leftLargest.right = node.right
+                }
+
+                if (parent == null) {
+                    root = leftLargest
+                } else if (parent.key > key) {
+                    parent.left = leftLargest
+                } else {
+                    parent.right = leftLargest
+                }
+            }
+            true
+        } else if(node.key > key) {
+            remove(node, node.left, key)
+        } else {
+            remove(node, node.right, key)
+        }
+    }
+
+    private fun findLargest(parent: BinarySearchTreeNode<K, V>, node: BinarySearchTreeNode<K, V>)
+    : Pair<BinarySearchTreeNode<K, V>, BinarySearchTreeNode<K, V>> {
+        if(node.right == null) {
+            return parent to node
+        }
+
+        return findLargest(node, node.right!!)
     }
 
     private fun search(node: BinarySearchTreeNode<K, V>?, key: K): V? {
